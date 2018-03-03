841b73ac1fffda5a85ec4ab6ecacf9bd13827274
Alexandr Tovmach <alexandrtovmach@gmail.com>
2018-03-03 03:40:46 +0200

    updated

--- js/script.js
+++ js/script.js
@@ -4,45 +4,38 @@ function init() {
         duration: 400
     });
 
-    new Waypoint.Inview({
+    new Waypoint({
         element: getBCL('knife_wrapper'),
-        entered: function(direction) {
-            getBCL('knife_wrapper').classList.add('startAnim')
+        handler: function(direction) {
+            getBCL('knife_wrapper').classList.toggle('startAnim')
         },
-        exited: function(direction) {
-            getBCL('knife_wrapper').classList.remove('startAnim')
-        }
+        offset: '75%'
     })
-    new Waypoint.Inview({
-        element: getBCL('stick_wrapper'),
-        enter: function(direction) {
-            getBCL('stick_wrapper').classList.add('startAnim')
+    new Waypoint({
+        element: getBCL('sticks'),
+        handler: function(direction) {
+            getBCL('sticks').classList.toggle('startAnim')
         },
-        exited: function(direction) {
-            getBCL('stick_wrapper').classList.remove('startAnim')
-        }
+        offset: '50%'
     })
-    new Waypoint.Inview({
-        element: getBCL('stick_textWrapper'),
-        enter: function(direction) {
-            getBCL('stick_textWrapper').classList.add('startAnim')
-        },
-        exited: function(direction) {
-            getBCL('stick_textWrapper').classList.remove('startAnim')
-        }
-    })
-    new Waypoint.Inview({
+    new Waypoint({
         element: getBCL('whiskey_wrapper'),
-        enter: function(direction) {
-            getBCL('whiskey_wrapper').classList.add('startAnim')
+        handler: function(direction) {
+            const ice = getBCL('icecube_wrapper');
+            const glass = getBCL('whiskey_wrapper');
+            if (ice.style.position) {
+                ice.style = null;
+            } else {
+                ice.style.position = 'absolute';
+                ice.style.top = window.pageYOffset + ice.clientHeight/2 + 'px';
+                ice.style.transition = 'transform 1s ease-in, opacity 1s 1s';
+                ice.style.transform = 'translateY(' + (glass.offsetTop + glass.clientHeight/2 + 20*2 + ice.clientHeight) + 'px)';
+                ice.style.opacity = '0';
+            }
+            // ice.classList.toggle('startAnim');
+            glass.classList.toggle('startAnim')
         },
-        exited: function(direction) {
-            getBCL('whiskey_wrapper').classList.remove('startAnim')
-        }
-    })
-
-    window.addEventListener('scroll', (event) => {
-        console.log(event)
+        offset: '75%'
     })
 
     setTimeout(plateAnimation, 2000)
